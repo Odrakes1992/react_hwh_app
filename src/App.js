0a3b58b73c132c67955091d6f10a3661fc691540
Osneil Drakes <osneil-drakes@hotmail.com>
2022-01-18 21:15:25 +0000

    updating styling and import bootstrap css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Home from "./Components/Home";
 import Results from "./Components/Results";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 import { useState } from "react";
-import Container from "react-bootstrap/Container";
+import styled from "styled-components";
+import { Button } from "react-bootstrap";
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
 class Home extends Component {
   // const [selectedFile, setSelectedFile] = useState();
@@ -8,31 +23,32 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <Container>
-          <h1 className="display-4"> Hinge Will Happen</h1>
+      <StyledDiv>
+        <div>
+          <h1 className="display-3"> Hinge Will Happen</h1>
+          <hr className="my-4" />
           <p>
             Ever wondered if there was some kinda Hinge yearly wrap up, just so
             you know how often you swiped left (or right) on those voice prompts
             ? <br></br>
             Well I wish Hinge could tell you but they dont.
             <br />I mean they do but who is really trying to parse through some
-            Json data? <br></br> Ok 😏, That last one was rhetorical, because
+            Json data? <br></br> Ok 😏 that last one was rhetorical, because
             that's exactly what we are doing.
           </p>{" "}
           <br></br>
-          <div>
+          <div classname="container">
             <input type="file" name="file" />
             <div>
               <button>Submit</button>
             </div>
           </div>
-          <p>
+          <p classname="paragraph">
             p.s this is in no way affiliated with the Hinge guys, just hoping
             that they dont send a cease and desist.{" "}
           </p>
-        </Container>
-      </div>
+        </div>
+      </StyledDiv>
     );
   }
 }
