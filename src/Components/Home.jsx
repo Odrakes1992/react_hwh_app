1886f29367ede7625ddb4f4b617d0bbd13c10610
Osneil Drakes <osneil-drakes@hotmail.com>
2022-01-18 23:47:44 +0000

    update home page / css/ copy

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { useState } from "react";
 import styled from "styled-components";
-import { Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import { MdSwipe } from "react-icons/md";
 
 const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  font-size: 85px;
+  margin-top: 10%;
 `;
 
 const StyledDiv = styled.div`
@@ -15,6 +18,19 @@ const StyledDiv = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
+  font-size: 18px;
+`;
+
+const FileInput = styled.input`
+  font-family: "IBM Plex Sans", sans-serif;
+`;
+
+const BottomView = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  bottom: 0;
+  position: relative;
 `;
 
 class Home extends Component {
@@ -24,29 +40,43 @@ class Home extends Component {
   render() {
     return (
       <StyledDiv>
-        <div>
-          <h1 className="display-3"> Hinge Will Happen</h1>
-          <hr className="my-4" />
+        <Header>
+          {" "}
+          Hinge Will Happen <MdSwipe />
+        </Header>
+        <hr className="my-4" />
+        <StyledDiv>
           <p>
-            Ever wondered if there was some kinda Hinge yearly wrap up, just so
-            you know how often you swiped left (or right) on those voice prompts
-            ? <br></br>
-            Well I wish Hinge could tell you but they dont.
-            <br />I mean they do but who is really trying to parse through some
-            Json data? <br></br> Ok 😏 that last one was rhetorical, because
-            that's exactly what we are doing.
+            Ever wondered if there was some kinda Hinge yearly wrap up.. <br />{" "}
+            Just so you know how often you swiped left (or right) on those voice
+            prompts ? Well <br /> I wish Hinge could tell you but they don't. I
+            mean they do but who is really trying to parse through <br />
+            some Json data. This is where we come in, give us the data and we
+            will make it make sense. <br />
+            <br />
+            By the way this is in no way affiliated with the Hinge guys, <br />{" "}
+            just hoping that they dont send a cease and desist ... styling is
+            merely coincidence.
           </p>{" "}
-          <br></br>
-          <div classname="container">
-            <input type="file" name="file" />
-            <div>
-              <button>Submit</button>
-            </div>
-          </div>
+        </StyledDiv>
+
+        <BottomView>
           <p classname="paragraph">
-            p.s this is in no way affiliated with the Hinge guys, just hoping
-            that they dont send a cease and desist.{" "}
-          </p>
+            Download your data and upload your <strong>'matches.json'</strong>{" "}
+            file
+          </p>{" "}
+        </BottomView>
+        <br></br>
+
+        <div>
+          <FileInput
+            type="file"
+            //onChange={}
+            accept="application/JSON"
+          />
+          <Button variant="dark" type="submit">
+            Submit
+          </Button>
         </div>
       </StyledDiv>
     );
