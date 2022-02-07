c594d6cbbfa336d359f2b09c4f90988f51452de6
Osneil Drakes <osneil-drakes@hotmail.com>
2022-02-07 18:36:57 +0000

    update app for bar chart and copy

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -21,10 +21,25 @@ let resultsHinge = {
   totalSeen: 0,
   totalLikes: 0,
   totalLeftSwipes: 0,
+  matches: 0,
+  openers: [],
+  frequency: {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+  },
 };
 
 let total = Object.keys(JsonData).length;
-let test = 0;
 
 // Testing results
 
@@ -45,10 +60,20 @@ let test = 0;
 // console.log(types.positive)
 
 const countItems = JsonData.map((info) => {
-  if (info.hasOwnProperty("like")) {
+  if (info.hasOwnProperty("like") && !info.hasOwnProperty("match")) {
     resultsHinge.totalLikes += 1;
+    // to refactor
+    let month = new Date(info.like[0].timestamp);
+    let getInt = month.getMonth();
+    resultsHinge.frequency[getInt] += 1;
   } else if (info.hasOwnProperty("block")) {
     resultsHinge.totalLeftSwipes += 1;
+    let month = new Date(info.block[0].timestamp);
+    let getInt = month.getMonth();
+    resultsHinge.frequency[getInt] += 1;
+  } else if (info.hasOwnProperty("match")) {
+    resultsHinge.matches += 1;
+    console.log(resultsHinge.matches);
   }
 });
 
@@ -66,6 +91,33 @@ const state = {
   ],
 };
 
+const frequency = {
+  labels: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  //labels: ["Yes", "No"],
+  datasets: [
+    {
+      label: "your activity throughout the year",
+      backgroundColor: ["#B21F00", "#00A6B4"],
+      hoverBackgroundColor: ["#501800", "#003350"],
+      //data: [65, 59, 80, 81, 56],
+      data: Object.values(resultsHinge.frequency),
+    },
+  ],
+};
+
 function Results() {
   return (
     <div>
@@ -74,13 +126,15 @@ function Results() {
       </h1>
       <hr className="my-4" />
       <p>
-        First and foremost, check the fun chart below that's how picky you were
-        pal! You swiped left on a total{" "}
+        First and foremost, check the fun chart below - that's how picky you
+        were pal! You swiped left on a total{" "}
         <strong>{resultsHinge.totalLeftSwipes}</strong> people, damn. No
         judgement here. <br />
         <br /> For a bit more quick maths that's{" "}
         {Math.floor((resultsHinge.totalLeftSwipes / total) * 100)}% not making
-        the cut.
+        the cut. You also matched with <strong>{resultsHinge.matches}</strong>{" "}
+        people, lucky you I guess 🧐 <br></br> Well you fared better than Todo
+        in season 1 of Jujutsu Kaisen, so I guess that's something.
       </p>
       {/* <Pie
         data={state}
@@ -102,7 +156,7 @@ function Results() {
           options={{
             title: {
               display: true,
-              text: "Average Rainfall per month",
+              text: "Swiping activity",
               fontSize: 20,
               //width={"30%"},
             },
@@ -114,6 +168,48 @@ function Results() {
           }}
         />
       </div>
+
+      <div>
+        <Bar
+          data={frequency}
+          options={{
+            title: {
+              display: true,
+              text: "Online Activity",
+              fontSize: 20,
+              //width={"30%"},
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+            maintainAspectRatio: false,
+          }}
+        />
+      </div>
+
+      <div>
+        <p>
+          {" "}
+          Ok, so you can see how active you were throughout the year, the
+          question is though was this a good return for the amount of swiping
+          that occurred....<br></br>
+          Ok that was rhetorical, but to help with this internal reflection
+          think about it this way your match to swipe ratio was roughly 1 match
+          for every{" "}
+          <strong>
+            {Math.floor((resultsHinge.matches / total) * 100)} swipes{" "}
+          </strong>
+          .
+        </p>
+      </div>
+
+      <p>
+        You were quite conversationalist, of the ones that you matched you had a
+        few back and forths. Our favourite opener was.... ok this is a joke it
+        wasn't our favourite opener, we just chose one at random. If it's a
+        funny one - you are welcome, if it's not well that's on you.
+      </p>
     </div>
   );
 }
